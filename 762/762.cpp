6fdd39d129d6e6a4371904787a498e1848084348
Zubayer Rahman <zubayer.csesust@gmail.com>
2020-05-08 00:12:07 +0600

    Meta Details Added.

---
+++ 762/762.cpp
@@ -0,0 +1,116 @@
+/**
+Online Judge : UVA Online Judge
+Problem No   : 762
+Problem Name : We Ship Cheap
+Author       : Zubayer Rahman
+Email        : zubayer.csesust@gmail.com
+Time Limit   : 3.000s
+CPU          : 0.033s
+Memory       :
+Algorithm    : BFS
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+map<string,vector<string>> mp;
+map<string,string> parent;
+map<string,int> visited;
+
+void __init__()
+{
+    mp.clear();
+    parent.clear();
+    visited.clear();
+}
+
+int _bfs(string _start, string _end)
+{
+    queue<string> q;
+
+    visited[_start]=1;
+    parent[_start]=_start;
+    q.push(_start);
+
+    int len,i;
+    string str,s;
+
+    while(!q.empty())
+    {
+        str=q.front();
+        q.pop();
+
+        len=mp[str].size();
+
+        for(i=0; i<len; i++)
+        {
+            s=mp[str][i];
+
+            if(!visited[s])
+            {
+                visited[s]=1;
+                q.push(s);
+                parent[s]=str;
+            }
+            if(s==_end)
+                break;
+        }
+
+        if(s==_end)
+            return 1;
+    }
+    return -1;
+}
+
+void printPath(string _start,string _end)
+{
+    if(_end==_start)
+        return;
+    printPath(_start,parent[_end]);
+    cout<<parent[_end]<<" "<<_end<<endl;
+}
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    freopen("input.txt", "r", stdin);
+#endif // ONLINE_JUDGE
+
+    int testCase;
+
+    bool newLine=false;
+
+    while(cin>>testCase)
+    {
+        __init__();
+
+        int i;
+        string str1,str2;
+
+        for(i=0; i<testCase; i++)
+        {
+            cin>>str1>>str2;
+
+            mp[str1].push_back(str2);
+            mp[str2].push_back(str1);
+
+            visited[str1]=0;
+            visited[str2]=0;
+        }
+
+        cin>>str1>>str2;
+
+        if(newLine)
+            cout<<endl;
+        newLine=true;
+
+        if(str1==str2)
+            cout<<str1<<" "<<str2<<endl;
+        else if(_bfs(str1,str2)==-1)
+            cout<<"No route"<<endl;
+        else
+            printPath(str1,str2);
+    }
+    return 0;
+}
