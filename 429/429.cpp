a33c58b33901d79eb6b6fbc0da0925bdd9afc58c
Zubayer Rahman <zubayer.csesust@gmail.com>
2020-08-07 02:10:41 +0600

    UVA problem 429 main File.

---
+++ 429/429.cpp
@@ -0,0 +1,24 @@
+/**
+Online Judge : UVA Online Judge
+Problem No   : 429
+Problem Name : Word Transformation
+Author       : Zubayer Rahman
+Email        : zubayer.csesust@gmail.com
+Time Limit   : 3.000s
+CPU          :
+Memory       :
+Algorithm    :
+*/
+
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+#ifndef ONLINE_JUDGE
+    fopen("429.txt", "r", stdin);
+#endif // ONLINE_JUDGE
+
+    return 0;
+}
