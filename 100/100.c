2f094e85e4a4701ff759222267ee3289ab9c85e6
Zubayer Rahman <zubayer.rahman@boomershub.com>
2021-01-26 04:39:59 +0600

    File Formatted

--- 100/100.c
+++ 100/100.c
@@ -10,7 +10,7 @@ Memory       :
 Algorithm    : Ad-hoc
 */
 
-#include<stdio.h>
+#include <stdio.h>
 
 int main()
 {
@@ -18,47 +18,48 @@ int main()
     freopen("input.txt", "r", stdin);
 #endif // ONLINE_JUDGE
 
-    int a,b,n,m=1,temp1,temp2,p,q,length,max=1,c,d;
-    while(scanf("%d %d",&a,&b)!=EOF)
+    int a, b, n, m = 1, temp1, temp2, p, q, length, max = 1, c, d;
+    while (scanf("%d %d", &a, &b) != EOF)
     {
-        max=1;
-        c=a;
-        d=b;
-        p=a;
-        q=b;
-        if(a>b)
+        max = 1;
+        c = a;
+        d = b;
+        p = a;
+        q = b;
+        if (a > b)
         {
-            p=b;
-            q=a;
+            p = b;
+            q = a;
         }
-        for(a=p; a<=q; a++)
+        for (a = p; a <= q; a++)
         {
-            n=a;
-            if(n==1)
-                m=1;
-            if(n>1)
+            n = a;
+            if (n == 1)
+                m = 1;
+            if (n > 1)
             {
-                m=1;
-                while(n!=1)
+                m = 1;
+                while (n != 1)
                 {
-                    if(n%2==1)
+                    if (n % 2 == 1)
                     {
-                        n=3*n+1;
+                        n = 3 * n + 1;
                         m++;
                     }
-                    if(n%2==0)
+                    if (n % 2 == 0)
                     {
-                        n=n/2;
+                        n = n / 2;
                         m++;
                     }
                 }
             }
-            length=m;
-            if(length>max)
-                max=length;
-            else length=length;
+            length = m;
+            if (length > max)
+                max = length;
+            else
+                length = length;
         }
-        printf("%d %d %d\n",c,d,max);
+        printf("%d %d %d\n", c, d, max);
     }
     return 0;
 }
